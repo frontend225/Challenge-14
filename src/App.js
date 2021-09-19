4222eb9ccddb1d3452d23c1d2b799233e2c6d96d
frontend225 <tfahmed@yahoo.com>
2021-09-18 17:53:20 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<main className="text-black-200 bg-black-400 body-font">
+      <About />
+      <section id="about">
+          <h1>
+            <title> Hi! I’m Tahmina Ahmed.</title>
+            </h1>
+<Contact />
+<section id= "contact">
+  <h2>
+    <title> Email: <tfahmed@yahoo.com </title>
+    </h2>
+   <head>
+   <title> Front End Web Development Resume</title>
+   </head>
+   <h3> PDF </h3>
+    
+    <title> My GitHub Profile </title>
+    <a href= "https://github.com/frontend225" </a> 
+
+<Projects />
+<section id= "projects">
+  <h3>
+    <title> Projects</title>
+    <a href= "https://github.com/CodeTommy/Project1" </a>
+</section>
+</main>
+  );
+}
+
+<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    </section>
+  );
+}
+
+export default App;
